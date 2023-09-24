fc62bb8cff30ed8aae94ec74ee0282e12228d3b4
kaifxworkz <145015099+kaifxworkz@users.noreply.github.com>
2023-09-24 22:24:55 +0530

    Create Fan.java
    Fan class

---
+++ Fan.java
@@ -0,0 +1,15 @@
+package Odc;
+
+public class Fan {
+
+	Fan(String name){
+		System.out.println("I AM IS KHAN");
+	}
+	
+		public static void main(String[] args) {
+		 new Fan("Kaif");
+		 new Fan("KAIF");
+
+	}
+
+}
