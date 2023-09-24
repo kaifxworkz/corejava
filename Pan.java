87e9887750808646ecb065a1827656f924046cfb
kaifxworkz <145015099+kaifxworkz@users.noreply.github.com>
2023-09-24 22:21:43 +0530

    Create Pan.java
    pan

---
+++ Pan.java
@@ -0,0 +1,14 @@
+package Odc;
+
+public class Pan {
+
+	Pan(int numbaer){
+		System.out.println("it my pan");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         new Pan (1);
+         new Pan (2);
+	}
+
+}
