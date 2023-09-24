ef61b865766ad50bed0b7a08634d53b00596e396
kaifxworkz <145015099+kaifxworkz@users.noreply.github.com>
2023-09-24 22:25:54 +0530

    Create Windown.java
    Windows class

---
+++ Windown.java
@@ -0,0 +1,17 @@
+package Odc;
+
+public class Windows {
+
+	Windows(int numbaer, int numbaer2){
+	     System.out.println(numbaer + numbaer);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         new Windows(16, 43);
+         new Windows(1,43);
+        
+         
+	}
+
+}
