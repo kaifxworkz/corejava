3f326ea9650c5981b3d5101bc9fa5168fb363417
kaifxworkz <145015099+kaifxworkz@users.noreply.github.com>
2023-09-24 22:23:42 +0530

    Create Boll.java
    Boll class

---
+++ Boll.java
@@ -0,0 +1,16 @@
+package Odc;
+
+public class Boll {
+	
+	
+	Boll(String Fast){
+		System.out.println("I am frist Later"+"K");
+	}
+	
+
+	public static void main(String[] args) {
+		 new Boll ("KAIF");
+		 new Boll ("KAIF");
+	}
+
+}
