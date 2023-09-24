28d8650e44a35a200ebca736ccce35a626b2798a
kaifxworkz <145015099+kaifxworkz@users.noreply.github.com>
2023-09-24 22:22:39 +0530

    Create Bag.java
    Bag classes

---
+++ Bag.java
@@ -0,0 +1,17 @@
+package Odc;
+
+public class Bag {
+
+	   Bag(int numbaer){
+		   System.out.println("it is my bag");
+	   }
+	
+	
+	public static void main(String[] args) {
+
+ 		   new Bag(3);
+ 		   new Bag(2);
+
+	}
+
+}
