9c3fc3a3fbca443715df75f60c545d03630aa70f
kaifxworkz <145015099+kaifxworkz@users.noreply.github.com>
2023-10-05 15:55:11 +0530

    Create Boy.java
    String

---
+++ Boy.java
@@ -0,0 +1,59 @@
+public class  Boy
+{
+	public static void main(String[] args)
+	{	
+		
+                		String value1="value 1";
+                		String value2="value 2";
+                		String value3="value 3";
+                		String value4="value 4";
+                		String value5="value 5";
+                		String value6="value 6";
+                		String value7="value 7";
+                		String value8="value 8";
+                		String value9="value 9";
+                		String value10="value 10";
+                                    		
+                                    		value1="value 6";
+                                    		value2="value 7";
+                                    		value3="value 8";
+                                    		value4="value 9";
+                                    		value5="value 10";
+		
+                            		String value11= new String("value 11");
+                            		String value12= new String("value 12");
+                            		String value13= new String("value 13");
+                            		String value14= new String("value 14");
+                            		String value15= new String("value 15");
+                            		String value16= new String("value 16");
+                            		String value17= new String("value 17");
+                            		String value18= new String("value 18");
+                            		String value19= new String("value 19");
+                            		String value20= new String("value 20");
+                            		
+          		
+          		value11="value 16";
+          		value12="value 17";
+          		value13="value 18";
+          		value14="value 19";
+          		value15="value 20";
+          	
+
+      		System.out.println(value1==value6);
+      		System.out.println(value2==value7);
+      		System.out.println(value3==value8);
+      		System.out.println(value4==value9);
+      		System.out.println(value5==value10);	
+      		System.out.println(value11==value16);
+      		System.out.println(value12==value17);
+      		System.out.println(value13==value18);
+      		System.out.println(value14==value19);
+      		System.out.println(value15==value20);
+      		System.out.println(value1==value11);
+      		System.out.println(value2==value12);
+      		System.out.println(value3==value13);
+      		System.out.println(value4==value14);
+      		System.out.println(value5==value15);
+      	}
+}
+	
