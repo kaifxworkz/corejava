a407b96bbce81a3750c7a1edd209d1e7548f993f
kaifxworkz <145015099+kaifxworkz@users.noreply.github.com>
2023-11-04 16:52:25 +0530

    Create ArrayLengthExampl.java
    test ArrayLengthExampl

---
+++ ArrayLengthExampl.java
@@ -0,0 +1,10 @@
+public class ArrayLengthExample {
+    public static void main(String[] args) {
+        int[] myArray = {12, 3, 2, 45, 7, 6}; 
+
+      
+        int lengthOfArray = myArray.length;
+
+        System.out.println("Length of the array is: " + lengthOfArray);
+    }
+}
